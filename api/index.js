17bc038bd9d175fd8e874b0f0433b3ba0fe5b474
Abhishek-Mishra-1503 <abhishek1502mishra@gmail.com>
2025-07-29 02:21:59 +0530

    connected to DB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();   // initializing dotenv
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Connected to DB");
+}).catch((err)=>{
+    console.log(err);
+});    //if no error than the 'then' part will run otherwise catch part will run
+
+// as when we share the code we dont want our username and password of database to be visible to others so we add the connection string in the .env file. The .env file is ignored and not pushed in the git because we add the .env file name in the .gitignore file
+
+// we cannot directly use a .env in the backend so we have to import the dotenv package to use it
 
 const app=express();
-
+  
 app.listen(3000,()=>{
     console.log('Server is running on port 3000!')
 })
